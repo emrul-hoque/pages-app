bc0271db9f3af1a4e451174116c9da4dd5ec32a3
Emrul <emrul.hoque@gmail.com>
2024-01-19 21:57:13 +0600

    Add files

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+
+# def home_page_view(request):
+#   return HttpResponse("Hello, World!")
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):  
+    template_name = "about.html"
+    
